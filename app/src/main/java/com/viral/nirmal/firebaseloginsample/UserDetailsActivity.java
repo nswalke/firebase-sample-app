a4919c41ca37e0df07ee2f18d3e6dcc3e7a06c9f
nswalke <nswalke@gmail.com>
2018-05-02 17:09:24 +0530

    Showed user details on screen in user details activity.

--- app/src/main/java/com/viral/nirmal/firebaseloginsample/UserDetailsActivity.java
+++ app/src/main/java/com/viral/nirmal/firebaseloginsample/UserDetailsActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -14,8 +15,13 @@ public class UserDetailsActivity extends AppCompatActivity implements View.OnCli
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_details);
+
         FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
-        ((TextView) findViewById(R.id.tv_user_details)).setText(String.valueOf(firebaseUser));
+        ((TextView) findViewById(R.id.tv_name)).setText("Name: " + String.valueOf(firebaseUser.getDisplayName()));
+        ((TextView) findViewById(R.id.tv_email)).setText("Email: " + String.valueOf(firebaseUser.getEmail()));
+        ((TextView) findViewById(R.id.tv_phone)).setText("Phone: " + String.valueOf(firebaseUser.getPhoneNumber()));
+        ((ImageView) findViewById(R.id.iv_profile_pic)).setImageURI(firebaseUser.getPhotoUrl());
+
         findViewById(R.id.btn_logout).setOnClickListener(this);
     }
 
